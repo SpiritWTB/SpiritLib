a4d4706bf71afd9da519ae3924bc4cae4b2f458e
JuiceMan <richylc@gmail.com>
2020-03-30 19:04:43 -0400

    Create SpiritLib_core.lua

---
+++ SpiritLib_core.lua
@@ -0,0 +1,16 @@
+﻿SpiritLib = {}
+
+local modules = {"Animation", "Weapons", "Attachment", "Pathfinding"}
+
+for k, v in pairs(modules) do
+    local loader = CreatePart(0, newVector3(0, 0, 0), newVector3(0, 0, 0))
+    loader.visible = false
+    loader.cancollide = false
+
+    -- Load module script
+    loader.script = v
+
+    loader.scripts[1].SpiritLib = This.scripts[1]
+
+    print("Loaded " .. v .. " module.")
+end
