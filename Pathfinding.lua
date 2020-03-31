46841d4ac69b21fa4d24af160864517984440756
JuiceMan <richylc@gmail.com>
2020-03-30 23:10:02 -0400

    Update Pathfinding.lua

--- Pathfinding.lua
+++ Pathfinding.lua
@@ -4,13 +4,6 @@ ModuleName = nil
 -- variables ModuleName and SpiritLib will be set before this runs
 function LoadModule()
 
-	-- make sure spiritlib is installed
-	local SpiritLibPart = PartByName("SpiritLib")
-	SpiritLib = SpiritLibPart.scripts[1]
-	if (SpiritLibPart == nil or SpiritLibPart.scripts[1] == nil) then print("PathFinding is part of SpiritLib, which cannot be found. Please install SpiritLib properly.") end
-
-	SpiritLib.PathFinding = {}
-
 
 
 
