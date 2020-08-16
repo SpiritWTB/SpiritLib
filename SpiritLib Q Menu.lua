29dec1d041f4c0c40f4b9fd43307f727d4656bbe
Alyx <25337749+amazingalyx@users.noreply.github.com>
2020-08-16 04:00:43 -0400

    Done fo today

--- SpiritLib Q Menu.lua
+++ SpiritLib Q Menu.lua
@@ -1,10 +1,18 @@
 local tabsHeight = 28
 local tabsPadding = 4
+
 local panelPadding = 4
+
+local buttonsSize = newVector2(160, 160)
+local buttonsPadding = 8
 
 local allTabs = {}
 local allTabsWidth = 0
 local currentTab
+
+local allButtons = {}
+local allButtonsSize = Vector2.zero
+local currentButton
 
 local mainWindowSize = newVector2(ScreenSize().x - 120, ScreenSize().y - 120)
 local mainWindowPos = newVector2(60, 60)
@@ -40,7 +48,7 @@ local function CreateTab(name, width)
 	panel.name = name .. " Panel"
 	panel.parent = mainWindow
 	panel.position = newVector2(panelPadding, panelPadding)
-	panel.color = temp
+	panel.color = Color.clear
 
 	allTabs[name] = {
 		button = button,
@@ -79,7 +87,32 @@ local function SelectTab(name)
 	end
 end
 
-local function CreateButton(panel, name)
+local function CreateButton(name, panel)
+	local holderSize = buttonsSize
+	local holderPos = allButtonsSize + newVector2(buttonsPadding, buttonsPadding)
+	local holder = MakeUIPanel(Vector2.zero, holderSize)
+	holder.name = name .. " Holder"
+	holder.parent = panel
+	holder.position = holderPos
+	holder.color = Color.clear
+
+	local button = MakeUIButton(Vector2.zero, holderSize, "<b>" .. name .. "</b>")
+	button.name = name
+	button.parent = holder
+	button.position = newVector2(0, 0)
+	button.textSize = 12
+	button.textAlignment = "MiddleCenter"
+
+	button.color = newColor(0.26, 0.26, 0.26, 1)
+	button.textColor = newColor(0, 0, 0, 1)
+
+	local realSize = buttonsSize + newVector2(buttonsPadding * 2, buttonsPadding * 2)
+
+	if allButtonsSize.x + realSize.x < panel.size.x - panel.position.x then
+		allButtonsSize = newVector2(allButtonsSize.x + realSize.x, allButtonsSize.y)
+	else
+		allButtonsSize = newVector2(0, allButtonsSize.y + realSize.y)
+	end
 end
 
 function OnUIButtonClick(button)
@@ -96,7 +129,12 @@ function Update()
 	end
 end
 
-CreateTab("Spawnlists", 80)
+local hello = CreateTab("Spawnlists", 80)
+
+for i = 1, 100 do
+	CreateButton("Test " .. i, hello)
+end
+
 CreateTab("Weapons", 80)
 CreateTab("Entities", 80)
 CreateTab("NPCs", 80)
