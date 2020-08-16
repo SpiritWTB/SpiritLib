f640f46b94b2d4c02aa477c2eb92f287d9f5cd50
Alyx <25337749+amazingalyx@users.noreply.github.com>
2020-08-16 03:04:27 -0400

    Ok

--- SpiritLib Q Menu.lua
+++ SpiritLib Q Menu.lua
@@ -14,38 +14,25 @@ mainWindow.enabled = false
 
 local tabsWindowSize = newVector2(0, tabsHeight + tabsPadding * 2)
 local tabsWindowPos = newVector2(0, -tabsWindowSize.y)
-local tabsWindow = MakeUIPanel(tabsWindowPos, tabsWindowSize, mainWindow)
+local tabsWindow = MakeUIPanel(Vector2.zero, tabsWindowSize)
+tabsWindow.name = "Tabs Window"
+tabsWindow.parent = mainWindow
+tabsWindow.position = tabsWindowPos
 tabsWindow.color = newColor(0.14, 0.14, 0.14, 0.98)
-
-local function SelectTab(name)
-	local selected = allTabs[name]
-
-	print(selected)
-
-	if selected and selected ~= currentTab then
-		currentTab.button.color = newColor(0.26, 0.26, 0.26, 1)
-		currentTab.button.textColor = newColor(0.6, 0.6, 0.6, 1)
-		currentTab.panel.enabled = false
-
-		currentTab = selected
-
-		currentTab.button.color = newColor(0.46, 0.46, 0.46, 1)
-		currentTab.buttontextColor = newColor(0.8, 0.8, 0.8, 1)
-		currentTab.panel.enabled = true
-	end
-end
 
 local function CreateTab(name, width)
 	local holderSize = newVector2(width, tabsHeight)
 	local holderPos = newVector2(allTabsWidth + tabsPadding, tabsPadding)
-	local holder = MakeUIPanel(holderPos, holderSize, tabsWindow)
+	local holder = MakeUIPanel(Vector2.zero, holderSize)
+	holder.name = name .. " Holder"
+	holder.parent = tabsWindow
+	holder.position = holderPos
 	holder.color = Color.clear
 
 	local button = MakeUIButton(Vector2.zero, holderSize, "<b>" .. name .. "</b>")
 	button.name = name
 	button.parent = holder
 	button.position = newVector2(0, 0)
-
 	button.textSize = 12
 	button.textAlignment = "MiddleCenter"
 
@@ -53,7 +40,7 @@ local function CreateTab(name, width)
 	panel.name = name .. " Panel"
 	panel.parent = mainWindow
 	panel.position = newVector2(panelPadding, panelPadding)
-	panel.color = Color.clear
+	panel.color = temp
 
 	allTabs[name] = {
 		button = button,
@@ -76,10 +63,28 @@ local function CreateTab(name, width)
 	return panel
 end
 
+local function SelectTab(name)
+	local selected = allTabs[name]
+
+	if selected and selected ~= currentTab then
+		currentTab.button.color = newColor(0.26, 0.26, 0.26, 1)
+		currentTab.button.textColor = newColor(0.6, 0.6, 0.6, 1)
+		currentTab.panel.enabled = false
+
+		currentTab = selected
+
+		currentTab.button.color = newColor(0.46, 0.46, 0.46, 1)
+		currentTab.buttontextColor = newColor(0.8, 0.8, 0.8, 1)
+		currentTab.panel.enabled = true
+	end
+end
+
+local function CreateButton(panel, name)
+end
+
 function OnUIButtonClick(button)
-	if button.parent == holder and button.parent.parent == mainWindow then
+	if button.parent and button.parent.parent and button.parent.parent.parent and button.parent.parent.parent == mainWindow then
 		SelectTab(button.name)
-		print("hurb")
 	end
 end
 
