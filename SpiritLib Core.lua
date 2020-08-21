c751ba6358bef0d3d4c854eeb55a774a89337ed3
Alyx <25337749+amazingalyx@users.noreply.github.com>
2020-08-21 18:54:14 -0400

    Cleanup

--- SpiritLib Core.lua
+++ SpiritLib Core.lua
@@ -1,58 +1,52 @@
 --[[
 SpiritLib Notes:
 
-You cannot pass certain types, (mainly functions, there may be others) between scripts 
+You cannot pass certain types, (mainly functions, there may be others) between scripts
 
 
 
 ]]
 
 SpiritLib = {}
-
 SpiritLib.Modules = {
-
 	["Core"] = This,
 
 	["Attachments"] = false,
-	["Models"] = false,
 	["Default Models"] = false,
+	["Models"] = false,
 	["Q Menu"] = false,
 	["Weapons"] = false
-
 }
 
 function Start()
 	This.name = "SpiritLib"
 
-	for moduleName, v in pairs(SpiritLib.Modules) do
-		local modulePart = CreatePart(0)
-		modulePart.visible = false
-		modulePart.cancollide = false
-		modulePart.name = "SpiritLib " .. moduleName
-		modulePart.script = "SpiritLib " .. moduleName
+	for name, active in pairs(SpiritLib.Modules) do
+		if not active then
+			local modulePart = CreatePart(0)
+			modulePart.visible = false
+			modulePart.cancollide = false
+			modulePart.name = "SpiritLib " .. name
+			modulePart.script = "SpiritLib " .. name
 
-		SpiritLib.Modules[moduleName] = modulePart
+			SpiritLib.Modules[name] = modulePart
 
-		print(moduleName .. " module loaded!")
+			print(name .. " module loaded!")
+		end
 	end
 end
 
 function FixedCall(moduleName, functionName, token, ...)
-	
-	local Module = SpiritLib.Modules[moduleName]
+	local activeModule = SpiritLib.Modules[moduleName]
 
 	-- make sure the module exists
-	if Module then
-		
+	if activeModule then
 		-- make sure the module has implemented ReceiveCall
-		if Module.scripts[1].Globals.ReceiveCall then
-
+		if activeModule.scripts[1].Globals.ReceiveCall then
 			-- make sure the module has the function you're trying to call
-			if Module.scripts[1].Globals[functionName] and type(Module.scripts[1].Globals[functionName]) == "function" then
-
-				Module.Call("ReturnCall", This, token, moduleName, functionName, ...)
+			if activeModule.scripts[1].Globals[functionName] and type(activeModule.scripts[1].Globals[functionName]) == "function" then
+				activeModule.Call("ReturnCall", This, token, moduleName, functionName, ...)
 				return This.table[token]
-
 			else
 				print([[CallModuleFunction: Module "]] .. moduleName .. [[" does not contain function "]] .. functionName .. [["]])
 			end
